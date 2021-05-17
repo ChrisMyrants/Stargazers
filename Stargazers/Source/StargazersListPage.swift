d306bf88be6c6a3d12d9fefca5f741746c645f73
Christian Miranti <christian.miranti@gmail.com>
2021-05-17 21:32:19 +0200

    Basic list and model for main page

--- Stargazers/Source/StargazersListPage.swift
+++ Stargazers/Source/StargazersListPage.swift
@@ -1,10 +1,35 @@
 import UIKit
 
+struct Stargazer {
+    let name: String
+}
+
 class StargazersListPage: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+        }
+    }
+    
+    let data: [Stargazer] = [
+        Stargazer(name: "Chris")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
 
-        // Do any additional setup after loading the view.
+extension StargazersListPage: UITableViewDataSource {
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = data[indexPath.row].name
+        
+        return cell
     }
 }
