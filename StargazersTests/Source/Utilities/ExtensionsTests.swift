6db9b6e747f13f34f7e243dc51284e0183a6d5cc
Christian Miranti <christian.miranti@gmail.com>
2021-05-24 10:00:54 +0200

    Add tests on get extension of optional

--- StargazersTests/Source/Utilities/ExtensionsTests.swift
+++ StargazersTests/Source/Utilities/ExtensionsTests.swift
@@ -2,13 +2,13 @@ import XCTest
 @testable import Stargazers
 
 final class ExtensionsTests: XCTestCase {
-    func testBoolExtension_NotMethod_FromTrueToFalse() {
+    func testBoolExtension_NotProperty_FromTrueToFalse() {
         
         /// GIVEN: a true boolean value
         let originalValue: Bool = true
         /// --------------------------------
         
-        /// WHEN: .not method on the true boolean is called
+        /// WHEN: .not property on the true boolean is called
         let sut = originalValue.not
         /// --------------------------------
         
@@ -17,18 +17,51 @@ final class ExtensionsTests: XCTestCase {
         /// --------------------------------
     }
     
-    func testBoolExtension_NotMethod_FromFalseToTrue() {
+    func testBoolExtension_NotProperty_FromFalseToTrue() {
         
         /// GIVEN: a false boolean value
         let originalValue: Bool = false
         /// --------------------------------
         
-        /// WHEN: .not method on the true boolean is called
+        /// WHEN: .not property on the true boolean is called
         let sut = originalValue.not
         /// --------------------------------
         
         /// THEN: the output is true
         XCTAssertTrue(sut)
+        /// --------------------------------
+    }
+    
+    func testOptional_GetMethod_GetWrappedValueOnSome() {
+        
+        /// GIVEN: an optional variable with some wrapped value inside
+        let expectedValue = "TEST"
+        let orValue = "not used"
+        let originalValue: String? = expectedValue
+        /// --------------------------------
+        
+        /// WHEN: .get method is called with any or value
+        let sut = originalValue.get(or: orValue)
+        /// --------------------------------
+        
+        /// THEN: the output is the original value
+        XCTAssertEqual(sut, expectedValue)
+        /// --------------------------------
+    }
+    
+    func testOptional_GetMethod_GetOrValueOnNone() {
+        
+        /// GIVEN: an optional variable with none value inside
+        let expectedValue = "TEST"
+        let originalValue: String? = nil
+        /// --------------------------------
+        
+        /// WHEN: .get method is called with a specific value
+        let sut = originalValue.get(or: expectedValue)
+        /// --------------------------------
+        
+        /// THEN: the output is the or value
+        XCTAssertEqual(sut, expectedValue)
         /// --------------------------------
     }
 }
